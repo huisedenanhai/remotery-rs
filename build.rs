51c510bd4f205aa07a910075bf9e80465499fa0f
unknown <winser@pku.edu.cn>
2021-01-05 01:02:34 +0800

    conditional compiling

--- build.rs
+++ build.rs
@@ -1,5 +1,25 @@
 fn main() {
-    cc::Build::new()
-        .file("Remotery/lib/Remotery.c")
-        .compile("remotery");
+    let mut build = cc::Build::new();
+
+    #[cfg(feature = "assume-little-endian")]
+    build.define("RMT_ASSUME_LITTLE_ENDIAN", "1");
+
+    #[cfg(feature = "cuda")]
+    build.define("RMT_USE_CUDA", "1");
+
+    #[cfg(feature = "dx11")]
+    build.define("RMT_USE_D3D11", "1");
+
+    #[cfg(feature = "metal")]
+    build
+        .define("RMT_USE_METAL", "1")
+        .file("Remotery/lib/RemoteryMetal.mm");
+
+    #[cfg(feature = "opengl")]
+    build.define("RMT_USE_OPENGL", "1");
+
+    #[cfg(feature = "posix-thread-names")]
+    build.define("RMT_USE_POSIX_THREADNAMES", "1");
+
+    build.file("Remotery/lib/Remotery.c").compile("remotery");
 }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ fn main() {
         let mut remotery: *mut Remotery = std::ptr::null_mut();
         _rmt_CreateGlobalInstance(&mut remotery as *mut *mut Remotery);
         loop {
-            let mut hash: rmtU32 = 0;
+            let mut hash: U32 = 0;
             let name = CString::new("hello rust").unwrap();
-            _rmt_BeginCPUSample(name.as_ptr(), rmtSampleFlags_RMTSF_None, &mut hash);
+            _rmt_BeginCPUSample(name.as_ptr(), SampleFlags::None as u32, &mut hash);
             _rmt_EndCPUSample();
         }
         _rmt_DestroyGlobalInstance(remotery);
