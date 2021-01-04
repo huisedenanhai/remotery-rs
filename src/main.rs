df180024cc024c824fa4d569a11f7d1abea5e260
huisedenanhai <winser@pku.edu.cn>
2021-01-04 22:55:42 +0800

    copy generated ffi to ffi.rs for later manual editing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
+mod ffi;
+use ffi::*;
 use std::ffi::CString;
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 fn main() {
     unsafe {
